ad82cf36ff2fff168242b5f97983c9999e16f41b
BESOoOoOo <b.almahmoodi@gmail.com>
2020-10-28 16:02:02 +0400

    Initial commit

---
+++ app/src/main/java/com/sheel/searchupdate/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sheel.searchupdate;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
